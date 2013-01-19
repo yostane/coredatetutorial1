381185e0cf565594d7c7d72b69419d807f540cf6
Yassine Benabbas <yassine.benabbas@gmail.com>
2013-01-19 19:55:30 +0100

    Premier commit

---
+++ CoreDataTutorial/Problem.m
@@ -0,0 +1,19 @@
+//
+//  Problem.m
+//  CoreDataTutorial
+//
+//  Created by Yassine Benabbas on 21/10/12.
+//  Copyright (c) 2012 yostane. All rights reserved.
+//
+
+#import "Problem.h"
+
+
+@implementation Problem
+
+@dynamic title;
+@dynamic level;
+@dynamic question;
+@dynamic solution;
+
+@end
